154e2db0aa159e0789cb262a5a92af7f6d2a6d69
RickCarr <rick.carr@telus.com>
2022-11-03 13:08:33 -0700

    Conditionals - whichSchool

---
+++ whichSchool.js
@@ -0,0 +1,16 @@
+const whichSchool  = function (age) {
+  console.log("I am " + age + ". Which school should I go to?");
+  if (age < 13) {
+    return "Elementary School"
+  }
+  else if (age >= 13 && age <= 18) {
+    return "Secondary School"
+  }
+  else 
+    return "Lighthouse Labs";
+
+
+}
+console.log(whichSchool(35)); 
+console.log(whichSchool(8)); 
+console.log(whichSchool(14)); 
