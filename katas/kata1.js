30e0357ae3c4e2ccbf5d9f14291db5a72f4c5e42
RickCarr <rick.carr@telus.com>
2022-11-08 19:07:50 -0800

    adding katas

---
+++ katas/kata1.js
@@ -0,0 +1,19 @@
+function sumLargestNumbers(data) {
+  if (data.length === 2) {
+    return data[0] + data[1];
+  } else {
+    let large1 = data[0];
+    let large2 = data[1];
+    for (let i of data) {
+      if (data[i] > large1 ) {
+        large1 = data[i];
+      } else if (data[i] > large2) {
+        large2 = data[i];
+      }
+    };
+    return large1 + large2;
+  }
+}
+console.log(sumLargestNumbers([1, 10]));
+console.log(sumLargestNumbers([1, 2, 3]));
+console.log(sumLargestNumbers([10, 4, 34, 6, 92, 2]));
---
+++ katas/kata2.js
@@ -0,0 +1,25 @@
+function conditionalSum(values, condition) {
+  let evenSum = 0;
+  let oddSum = 0;
+  if (condition === "even") {
+    for (let i of values) {
+      if (i % 2 === 0) {
+        evenSum += i;
+      }      
+    }
+    return evenSum;
+  } 
+  if (condition === "odd") {
+    for (let i of values) {
+      if (i % 2 !== 0) {
+        oddSum += i;
+      }      
+    }
+    return oddSum;
+  }  
+};
+
+console.log(conditionalSum([1, 2, 3, 4, 5], "even"));
+console.log(conditionalSum([1, 2, 3, 4, 5], "odd"));
+console.log(conditionalSum([13, 88, 12, 44, 99], "even"));
+console.log(conditionalSum([], "odd"));
